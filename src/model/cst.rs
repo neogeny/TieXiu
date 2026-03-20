dd08563610a4254ed5a58142bde6bec4758d18d0
apalala <apalala@gmail.com>
2026-03-20 16:50:58 -0400

    [cst] new version with generics instead of Box<> arguments

--- src/model/cst.rs
+++ src/model/cst.rs
@@ -3,20 +3,20 @@ use std::any::Any;
 pub type Node = Box<dyn Any>;
 
 pub enum Cst {
-    List(Vec<Node>), // The "Open" list
-    Item(Node),      // A single item or a "Closed" list
+    List(Vec<Node>),
+    Item(Node),
 }
 
-/// Equivalent to cstfinal()
 pub fn cst_final(cst: Cst) -> Cst {
     match cst {
-        // To "Close" a list, we wrap the Vec into a single Node
         Cst::List(v) => Cst::Item(Box::new(v)),
         item => item,
     }
 }
 
-pub fn cst_add(cst: Option<Cst>, node: Node, as_list: bool) -> Cst {
+pub fn cst_add<T: Any>(cst: Option<Cst>, item: T, as_list: bool) -> Cst {
+    let node: Node = Box::new(item);
+
     match (cst, as_list) {
         (None, true) => Cst::List(vec![node]),
         (None, false) => Cst::Item(node),
@@ -28,29 +28,37 @@ pub fn cst_add(cst: Option<Cst>, node: Node, as_list: bool) -> Cst {
     }
 }
 
-pub fn cst_merge(cst: Option<Cst>, other: Option<Cst>) -> Option<Cst> {
-    match (cst, other) {
-        (None, None) => None,
-        (None, Some(o)) => Some(o),
-        (Some(c), None) => Some(c),
-        // Both are open lists: extend
-        (Some(Cst::List(mut c_vec)), Some(Cst::List(o_vec))) => {
-            c_vec.extend(o_vec);
-            Some(Cst::List(c_vec))
+pub fn cst_merge<T: Any>(cst: Option<Cst>, other: T) -> Cst {
+    // We start by boxing the 'other' item.
+    let mut other_any: Node = Box::new(other);
+
+    // If 'other' is already a Cst, we extract it to handle merging/flattening.
+    // Otherwise, we treat it as a raw Node (Item).
+    let other_cst = if let Some(extracted) = other_any.downcast::<Cst>().ok() {
+        *extracted
+    } else {
+        Cst::Item(other_any)
+    };
+
+    match (cst, other_cst) {
+        (None, right) => right,
+
+        (Some(Cst::List(mut l_vec)), Cst::List(r_vec)) => {
+            l_vec.extend(r_vec);
+            Cst::List(l_vec)
         }
-        // Left is open, right is an item
-        (Some(Cst::List(mut c_vec)), Some(Cst::Item(o))) => {
-            c_vec.push(o);
-            Some(Cst::List(c_vec))
+        (Some(Cst::List(mut l_vec)), Cst::Item(r_node)) => {
+            l_vec.push(r_node);
+            Cst::List(l_vec)
         }
-        // Left is item, right is open
-        (Some(Cst::Item(c)), Some(Cst::List(mut o_vec))) => {
-            let mut new_vec = Vec::with_capacity(o_vec.len() + 1);
-            new_vec.push(c);
-            new_vec.append(&mut o_vec);
-            Some(Cst::List(new_vec))
+        (Some(Cst::Item(l_node)), Cst::List(mut r_vec)) => {
+            let mut new_vec = Vec::with_capacity(r_vec.len() + 1);
+            new_vec.push(l_node);
+            new_vec.append(&mut r_vec);
+            Cst::List(new_vec)
         }
-        // Both are items
-        (Some(Cst::Item(c)), Some(Cst::Item(o))) => Some(Cst::List(vec![c, o])),
+        (Some(Cst::Item(l_node)), Cst::Item(r_node)) => {
+            Cst::List(vec![l_node, r_node])
+        }
     }
 }
