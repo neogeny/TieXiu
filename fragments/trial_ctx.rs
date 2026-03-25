292b5f074525d2717cf8e7660997dc1189346805
apalala <apalala@gmail.com>
2026-03-25 09:49:08 -0400

    [fragments] more ideas in fragments of code

---
+++ fragments/trial_ctx.rs
@@ -0,0 +1,23 @@
+impl<'c> Ctx<'c> {
+    pub fn group<F, T>(&mut self, body: F) -> Result<T, String>
+    where
+        F: FnOnce(&mut Self) -> Result<T, String>,
+    {
+        // 1. Create a "Trial" version of the context (copy/clone)
+        let mut trial = self.clone();
+
+        // 2. Execute the grammar block on the trial context
+        match body(&mut trial) {
+            Ok(parsed) => {
+                // 3. SUCCESS: "Merge" by simply overwriting our state
+                *self = trial;
+                Ok(parsed)
+            }
+            Err(err) => {
+                // 4. FAILURE: Do nothing. 'trial' is dropped.
+                // Our 'self' is still at the pre-group position.
+                Err(err)
+            }
+        }
+    }
+}
