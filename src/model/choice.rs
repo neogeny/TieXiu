5be70b76a3c809d4b74b53f4c3499b7a76a3bfa0
apalala <apalala@gmail.com>
2026-03-25 09:48:46 -0400

    [model] get rid of ChoiceOption

--- src/model/choice.rs
+++ src/model/choice.rs
@@ -2,33 +2,19 @@ use crate::engine::cst::Cst;
 use crate::engine::ctx::Ctx;
 use super::model::Model;
 
-pub struct ChoiceOption {
-    exp: Box<dyn Model>
-}
 
-pub(crate) struct Choice {
-    pub options: Vec<ChoiceOption>,
+pub struct Choice {
+    pub options: Vec<Box<dyn Model>>,
 }
 
 impl Model for Choice {
     fn parse(&self, ctx: Ctx) -> Result<(Ctx, Cst), String> {
         for option in &self.options {
-            if let Ok((ctx, cst)) = option.parse(ctx) {
-                return Ok((ctx, cst));
+            // Because Ctx is Copy/Value, 'ctx' here is the same for every iteration
+            if let Ok((next_ctx, cst)) = option.parse(ctx) {
+                return Ok((next_ctx, cst));
             }
         }
         Err("No option matched".to_string())
-    }
-}
-
-impl ChoiceOption {
-    pub fn new(exp: Box<dyn Model>) -> Self {
-        Self { exp }
-    }
-}
-
-impl Model for ChoiceOption {
-    fn parse(&self, ctx: Ctx) -> Result<(Ctx, Cst), String> {
-        self.exp.parse(ctx)
     }
 }
