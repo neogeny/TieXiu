82102a2c2a63772209ebedb13fabf40c48fdaf51
apalala <apalala@gmail.com>
2026-03-30 09:35:39 -0400

    [grammars] try lifetime erasure on Group

--- src/grammars/group.rs
+++ src/grammars/group.rs
@@ -4,38 +4,47 @@
 use crate::contexts::{Cst, Ctx};
 use super::model::{CanParse, ParseResult};
 
-#[derive(Debug, Clone)]
-pub struct Group<'g, C> {
-    pub exp: Box<&'g dyn CanParse<C>>,
+#[derive(Debug)]
+pub struct Group<C> {
+    pub exp: Box<dyn CanParse<C>>,
 }
 
-impl<'g, C: Ctx> Group<'g, C> {
-    fn new(exp: &'g dyn CanParse<C>) -> Self {
+impl<C: Ctx> Group<C> {
+    pub fn new<T>(exp: T) -> Self
+    where
+        T: CanParse<C> + 'static,
+    {
         Self {
             exp: Box::new(exp),
         }
     }
 }
 
-impl<'g, C: Ctx> CanParse<C> for Group<'g, C>
-{
+impl<C: Ctx> CanParse<C> for Group<C> {
     fn parse(&self, ctx: C) -> ParseResult<C> {
         self.exp.parse(ctx)
     }
 }
 
-
-#[derive(Debug, Clone)]
-pub struct SkipGroup<'g, C> {
-    pub exp: Box<&'g dyn CanParse<C>>,
+#[derive(Debug)]
+pub struct SkipGroup<C> {
+    pub exp: Box<dyn CanParse<C>>,
 }
 
-impl<'g, C: Ctx> CanParse<C> for SkipGroup<'g, C>
-{
+impl<C: Ctx> SkipGroup<C> {
+    pub fn new<T>(exp: T) -> Self
+    where
+        T: CanParse<C> + 'static,
+    {
+        Self {
+            exp: Box::new(exp),
+        }
+    }
+}
+
+impl<C: Ctx> CanParse<C> for SkipGroup<C> {
     fn parse(&self, ctx: C) -> ParseResult<C> {
         let (new_ctx, _) = self.exp.parse(ctx)?;
         Ok((new_ctx, Cst::Nil))
     }
 }
-
-
