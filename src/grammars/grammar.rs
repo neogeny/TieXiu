e44a69f08ff90e6c9cae0f5f526b96ae1d515503
apalala <apalala@gmail.com>
2026-03-31 18:21:29 -0400

    [grammars] add left-recursion analysis to the constructor

--- src/grammars/grammar.rs
+++ src/grammars/grammar.rs
@@ -9,3 +9,16 @@ pub struct Grammar<'g> {
     pub name: &'g str,
     pub rulemap: HashMap<&'g str, Rule<'g>>,
 }
+
+impl<'g> Grammar<'g> {
+    pub fn new(name: &'g str, rules: &[Rule<'g>]) -> Self {
+        let rulemap = rules
+            .iter()
+            .cloned()
+            .map(|r| (r.name, r))
+            .collect();
+
+        let grammar = Self { name, rulemap };
+        super::leftrec::mark_left_recursion(grammar)
+    }
+}
