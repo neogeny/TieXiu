b4f577234dd0fc9b1080739907d6ec2014eeeac6
apalala <apalala@gmail.com>
2026-04-03 11:14:57 -0400

    [repeat] &dyn Parser  -> &E

--- src/model/repeat.rs
+++ src/model/repeat.rs
@@ -1,30 +1,31 @@
 // Copyright (c) 2026 Juancarlo Añez (apalala@gmail.com)
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use super::parser::{Parser, S};
+use super::parser::S;
 use crate::astree::Cst;
 use crate::context::Ctx;
+use crate::model::E;
 
-pub fn skip_exp<C: Ctx>(exp: &dyn Parser<C>, ctx: C) -> Result<C, C> {
+pub fn skip_exp<C: Ctx>(exp: &E, ctx: C) -> Result<C, C> {
     match exp.parse(ctx) {
         Ok(S(new_ctx, _)) => Ok(new_ctx),
-        Err(err) => Err(err),
+        Err(err_ctx) => Err(err_ctx),
     }
 }
 
-pub fn add_exp<C: Ctx>(exp: &dyn Parser<C>, ctx: C, res: &mut Vec<Cst>) -> Result<C, C> {
+pub fn add_exp<C: Ctx>(exp: &E, ctx: C, res: &mut Vec<Cst>) -> Result<C, C> {
     match exp.parse(ctx) {
         Ok(S(new_ctx, cst)) => {
             res.push(cst);
             Ok(new_ctx)
         }
-        Err(err) => Err(err),
+        Err(err_ctx) => Err(err_ctx),
     }
 }
 
-pub fn repeat<C: Ctx>(exp: &dyn Parser<C>, mut ctx: C, res: &mut Vec<Cst>) -> C {
+pub fn repeat<C: Ctx>(exp: &E, mut ctx: C, res: &mut Vec<Cst>) -> C {
     loop {
-        match add_exp(exp, ctx, res) {
+        match add_exp(exp, ctx.clone(), res) {
             Ok(new_ctx) => ctx = new_ctx,
             Err(err_ctx) => return err_ctx,
         }
@@ -32,23 +33,23 @@ pub fn repeat<C: Ctx>(exp: &dyn Parser<C>, mut ctx: C, res: &mut Vec<Cst>) -> C 
 }
 
 pub fn repeat_with_pre<C: Ctx>(
-    exp: &dyn Parser<C>,
-    pre: &dyn Parser<C>,
+    exp: &E,
+    pre: &E,
     mut ctx: C,
     res: &mut Vec<Cst>,
     keep_pre: bool,
 ) -> C {
     loop {
-        match pre.parse(ctx) {
+        match pre.parse(ctx.clone()) {
             Err(err_ctx) => return err_ctx,
             Ok(S(new_ctx, pre_cst)) => match exp.parse(new_ctx) {
                 Err(err_ctx) => return err_ctx,
-                Ok(S(new_ctx, exp_cst)) => {
+                Ok(S(final_ctx, exp_cst)) => {
                     if keep_pre {
                         res.push(pre_cst);
                     }
                     res.push(exp_cst);
-                    ctx = new_ctx;
+                    ctx = final_ctx;
                 }
             },
         }
